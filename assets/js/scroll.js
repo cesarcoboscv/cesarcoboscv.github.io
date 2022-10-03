ad0a20c627ea1078a08f8e8c082428e630fb5533
Cesar Cobos <cesarcoboscv@gmail.com>
2022-10-03 13:47:29 -0500

    Agregada la sección de trabajo. Corrección de errores de visualización. En espera de más contenido

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -3,16 +3,10 @@ var resumebtn = document.getElementById("resumebtn");
 
 window.onscroll = function () {
     if (document.documentElement.scrollTop >= 80) {
-        navBar.classList.remove("bg-[#141E30]");
-        navBar.classList.add("bg-slate-900");
+        navBar.style.backgroundColor="#1e293b";
         navBar.classList.add("text-white");
-        
-        // resumebtn.style.visibility = "hidden";
     } else {
-        navBar.classList.remove("bg-slate-900");
-        navBar.classList.add("bg-[#141E30]");
+        navBar.style.backgroundColor="#141E30";
         navBar.classList.remove("text-white");
-        // resumebtn.style.visibility = "visible";
-
     }
 }
