899c93f8d24394a044b19d94ab4098b0ccd41867
Cesar Cobos <cesarcoboscv@gmail.com>
2022-11-04 11:57:11 -0600

    Reducción de código

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 let herobg = document.getElementById("hero");
+let herolayer = document.getElementById("hero-layer")
 let navBar = document.getElementById("full-navbar");
 
 // Device detection
@@ -35,6 +36,7 @@ window.onscroll = function(){
 window.addEventListener("scroll", function () { 
     let con= window.scrollY
     herobg.style.backgroundPosition= '50% '+  ((con * -.5 )  + 'px');
+    // herobg.style.filter = 'grayscale(100%)';
 })
 
 //Hero background
@@ -47,15 +49,17 @@ class bgImage{
     constructor(url){
         this.url = url;}
 }
-// let hero01 = new bgImage("https://drive.google.com/uc?export=view&id=1WEAdQ9j5Oq-VQ1bgkKjzUqOA_AE8jxCC")
+let hero01 = new bgImage("https://drive.google.com/uc?export=view&id=134jjzLe4vuQAymL8MNZARW7ABFtPORmw")
 let hero02 = new bgImage("https://drive.google.com/uc?export=view&id=1LNBWkmDbz-SS4DCX-gAhVbPV_1rqM2vF")
 let hero03 = new bgImage("https://drive.google.com/uc?export=view&id=1v9lzPnWZ8gguzP6FOtYQEm17znUveCuz")
-// let hero04 = new bgImage("https://drive.google.com/uc?export=view&id=1PRrJl8y9u9rElpXV7XmYi8pK86-kev84")
+let hero04 = new bgImage("https://drive.google.com/uc?export=view&id=1PRrJl8y9u9rElpXV7XmYi8pK86-kev84")
 let hero05 = new bgImage("https://drive.google.com/uc?export=view&id=1CDXToGqqCJN7K2jwm8JH7pD58c6lQT4x")
 let hero06 = new bgImage("https://drive.google.com/uc?export=view&id=1wP3cZOfbyLJjywGDI3gkRxSRgAlkssSx")
+let hero07 = new bgImage("https://drive.google.com/uc?export=view&id=1oAvNI2McpbrmDZXK2NfytrZcVrQBFzG0")
+let hero08 = new bgImage("https://drive.google.com/uc?export=view&id=1f1Gmj2J6tlVUNXvU7TkhdgU3ZpIMJk8N")
 
 
-bgimages.push(hero02,hero03,hero05,hero06);
+bgimages.push(hero01,hero02,hero03,hero04,hero05,hero06,hero07,hero08);
 
 function randomImage(){
     let randomImage = randomNumber(0, bgimages.length -1);
